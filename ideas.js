31356abe0f61735060c4e498b79b7b3782cf8fd4
tinabeattysmith <tinatest2020@gmail.com>
2020-10-03 14:39:44 -0400

    10/3 midday

--- ideas.js
+++ ideas.js
@@ -1,8 +1,5 @@
-const states = [{"state":"Alabama"},{"state":"Alaska"},{"state":"Arizona"},{"state":"Arkansas"},{"state":"California"},{"state":"Colorado"},{"state":"Connecticut"},{"state":"Delaware"},{"state":"Florida"},{"state":"Georgia"},{"state":"Hawaii"},{"state":"Idaho"},{"state":"Illinois"},{"state":"Indiana"},{"state":"Iowa"},{"state":"Kansas"},{"state":"Kentucky"},{"state":"Louisiana"},{"state":"Maine"},{"state":"Maryland"},{"state":"Massachusetts"},{"state":"Michigan"},{"state":"Minnesota"},{"state":"Mississippi"},{"state":"Missouri"},{"state":"Montana"},{"state":"Nebraska"},{"state":"Nevada"},{"state":"New Hampshire"},{"state":"New Jersey"},{"state":"New Mexico"},{"state":"New York"},{"state":"North Carolina"},{"state":"North Dakota"},{"state":"Ohio"},{"state":"Oklahoma"},{"state":"Oregon"},{"state":"Pennsylvania"},{"state":"Rhode Island"},{"state":"South Carolina"},{"state":"South Dakota"},{"state":"Tennessee"},{"state":"Texas"},{"state":"Utah"},{"state":"Vermont"},{"state":"Virginia"},{"state":"Washington"},{"state":"Washington, D.C."},{"state":"West Virginia"},{"state":"Wisconsin"},{"state":"Wyoming"}];
-
-const city = [{"city":"Anderson"},{"city":"Beech Grove"},{"city":"Bloomington"},{"city":"Broad Ripple"},{"city":"Brownsburg"},{"city":"Burns Harbor"},{"city":"Columbus"},{"city":"Dale"},{"city":"Elkhart"},{"city":"Evansville"},{"city":"Ferdinand"},{"city":"Fishers"},{"city":"Fort Wayne"},{"city":"Gary"},{"city":"Granger"},{"city":"Greencastle"},{"city":"Hammond"},{"city":"Harlan"},{"city":"Huntingburg"},{"city":"Indianapolis"},{"city":"Jasper"},{"city":"Kokomo"},{"city":"Lafayette"},{"city":"Lawrence"},{"city":"Mount Vernon"},{"city":"North Terre Haute"},{"city":"Ogden Dunes"},{"city":"Portage"},{"city":"Santa Claus"},{"city":"South Bend"},{"city":"Terre Haute"},{"city":"Warren Park"}];
-
-const currentWeather = {"city":"Indianapolis","state":"Indiana","country":"USA","location":{"type":"Point","coordinates":[-86.2147,39.7889]},"current":{"weather":{"ts":"2020-10-03T00:00:00.000Z","tp":12,"pr":1021,"hu":50,"ws":2.1,"wd":350,"ic":"04n"},"pollution":{"ts":"2020-10-03T00:00:00.000Z","aqius":12,"mainus":"p2","aqicn":4,"maincn":"p2"}}}
+let url = 'http://api.airvisual.com/v2/states?country=usa&key=88685f1b-f5be-478c-a063-f6ef94dfad42';
+let City;
 
 //targets the DOM element
 let dropDownState = document.getElementById("dropdownStateButton");
@@ -14,20 +11,37 @@ dropDownState.length=0;
 dropDownCity.length=0;
 
 //add eventlisteners and calls the functions to complete actions
-dropDownState.addEventListener("click", popState);
-dropDownCity.addEventListener("click", popCity);
-btnSubmit.addEventListener("click", submitStateCity);
+dropDownState.addEventListener('click', fetchResults);
+//dropDownCity.addEventListener("click", popCity);
+//btnSubmit.addEventListener("click", submitStateCity);
 
-function popState() {
-    let optionState;
-    //populates State dropdown
+function fetchResults(e) {
+    e.preventDefault();
+ 
+     fetch(url)
+        .then (function(result) {
+            return result.json();
+        })
+        .then (function(json) {
+            console.log("JSON: ", json);
+            popState(json);
+        });
+    
+function popState(json) {
+	let states = json.data;
+	console.log(states);
+	 //populates State dropdown
+    let optionState;   
     for (let i = 0; i < states.length; i++) {
-      optionState = document.createElement('option');
-      optionState.text = states[i].state;
-      dropDownState.appendChild(optionState);}
-};
+      	optionState = document.createElement('option');
+		optionState.text = states[i].state;
+		dropDownState.appendChild(optionState);}
 
-function popCity() {
+
+	};
+
+
+/*function popCity() {
     //populates City dropdown
     //? Research how to consider state in order to pop city. Will this require a second fetch?
     let optionCity;
@@ -36,40 +50,7 @@ function popCity() {
       optionCity.text = city[i].city;
       dropDownCity.appendChild(optionCity);}
 };
-
 function submitStateCity() {
 // get State/ City
-
-
-}
-
-//! sample of fetch chain.  Will need to figure this out becuase I think this will be needed since the city fetch requires state and current weather requires both city and state.
-let State;
-let City;
-
-// Call the API
-fetch('https://jsonplaceholder.typicode.com/posts/5').then(function (response) {
-	if (response.ok) {
-		return response.json();
-	} else {
-		return Promise.reject(response);
-	}
-}).then(function (data) {
-
-	// Store the post data to a variable
-	post = data;
-
-	// Fetch another API
-	return fetch('https://jsonplaceholder.typicode.com/users/' + data.userId);
-
-}).then(function (response) {
-	if (response.ok) {
-		return response.json();
-	} else {
-		return Promise.reject(response);
-	}
-}).then(function (userData) {
-	console.log(post, userData);
-}).catch(function (error) {
-	console.warn(error);
-});
+}*/
+};
--- index.js
+++
@@ -1,3 +0,0 @@
-let baseURL = 'https://api.airvisual.com/v2/country=usa?key=';
-let apiKey = '88685f1b-f5be-478c-a063-f6ef94dfad42';
-
